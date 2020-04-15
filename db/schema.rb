904a119a870b9c3dfeeb93402eb01f2e1e265b02
Juliana Ariffin <juliana@fastmail.com>
2020-04-15 12:54:47 +0200

    Tasks app done but not yet removed Completed in new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_062930) do
+ActiveRecord::Schema.define(version: 2020_04_15_080615) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
